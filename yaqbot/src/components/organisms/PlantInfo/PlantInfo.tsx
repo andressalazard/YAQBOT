1dd0cac5f6773788eb3747cc77c5a858dcc49831
Andrés Salazar <andres.salazar.d.18@gmail.com>
2025-11-07 20:38:51 -0500

    added a toggle component into the plants detail page

--- yaqbot/src/components/organisms/PlantInfo/PlantInfo.tsx
+++ yaqbot/src/components/organisms/PlantInfo/PlantInfo.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PlantDetailCard from '../../molecules/PlantDetailCard/PlantDetailCard';
 import DescriptionRack from '../DescriptionRack/DescriptionRack';
 import { descriptionType } from '../../../models/dataModel';
 import styles from './PlantInfo.module.css';
+import Toggle from '../../atoms/Toogle/Toogle';
+import ToggleOption from '../../molecules/ToggleOption/ToggleOption';
 
 interface PlantInfoProps {
   image: string;
@@ -17,10 +19,21 @@ interface PlantInfoProps {
 
 const PlantInfo: React.FC<PlantInfoProps> = ({ image, plant, details }) => {
   const { name, nickname, status, type } = plant;
+
+  const options = [
+    { label: 'Mostrar esta planta como pública', isToggle: false },
+    { label: 'Activar el autoregado de la planta', isToggle: true },
+  ];
+
   return (
     <div className={styles.content}>
       <PlantDetailCard name={name} nickname={nickname} status={status} type={type} image={image} />
-      <DescriptionRack descriptionList={details} />
+      <div>
+        <DescriptionRack descriptionList={details} />
+        {options.map((option, index) => (
+          <ToggleOption key={index} label={option.label} isToggle={option.isToggle} />
+        ))}
+      </div>
 
       {/* <section className={styles.plant_description}>
         Here goes the details of the plant
